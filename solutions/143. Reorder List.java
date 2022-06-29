9e07843cc82d7380591e88f94ebc6374d0d3a3f6
Debadutta Panda <debadebaduttapanda.7@gmail.com>
2022-06-29 11:55:56 +0530

    4 ms

--- solutions/143. Reorder List.java
+++ solutions/143. Reorder List.java
@@ -1,22 +1,3 @@
-            head=ptr;
-        }
-        return head;
-    }
-    public void reorderList(ListNode head) {
-       ListNode firstList=null;
-        ListNode secondList=null;
-        ListNode temp=head;
-        int count=0;
-        while(temp!=null)
-        {
-            count++;
-            temp=temp.next;
-        }
-        temp=head;
-        int mid=count%2==0?(count/2):((count+1)/2);
-        count=1;
-        ListNode rareFirst=null,rareSecond=null;
-        while(temp!=null)
         {
             if(count<=mid)
             {
