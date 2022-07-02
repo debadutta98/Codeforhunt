d661a7ca737883cab9d7936fbf2d5fbcd19cea9a
Debadutta Panda <debadebaduttapanda.7@gmail.com>
2022-07-02 14:35:53 +0530

    0 ms

---
+++ solutions/704. Binary Search.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l=0,r=nums.length-1;
+        while(l<=r)
+        {
+            int mid=l+(r-l)/2;
+            if(nums[mid]==target)    
+            {   
+                return mid;
+            }
+            else if(nums[mid]>target)
+            {
+                r=mid-1;
+            }
+            else if(nums[mid]<target)
+            {
+                l=mid+1;
+            }
+        }
+        return -1;
+    }
+}
