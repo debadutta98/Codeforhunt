ba6e44d94c4a3f37b7ebdea5403eb726f10809dd
Debadutta Panda <debadebaduttapanda.7@gmail.com>
2022-08-05 12:41:25 +0530

    3 ms

---
+++ solutions/1720. Decode XORed Array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] decode(int[] encoded, int first) {
+        int arr[]=new int[encoded.length+1];
+        arr[0]=first;
+        for(int i=1;i<arr.length;i++)
+        {
+            arr[i]=(arr[i-1]^encoded[i-1]);
+        }
+        return arr;
+    }
+}
