5db77a725ea0ece203a9fbb5a100ae75a01e7875
Debadutta Panda <debadebaduttapanda.7@gmail.com>
2022-08-09 17:07:05 +0530

    7 ms

---
+++ solutions/946. Validate Stack Sequences.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stack=new Stack<>();
+        int j=0;
+        for(int i=0;i<pushed.length;i++)
+        {
+            stack.push(pushed[i]);
+            while(j<pushed.length && !stack.isEmpty() && stack.peek()==popped[j])
+            {
+                stack.pop();
+                j++;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
