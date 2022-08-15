5157d5493ab0b2f89c92afa89b6a0ecc546740d3
Debadutta Panda <debadebaduttapanda.7@gmail.com>
2022-08-15 22:35:41 +0530

    114 ms

--- solutions/2279. Maximum Bags With Full Capacity of Rocks.java
+++ solutions/2279. Maximum Bags With Full Capacity of Rocks.java
@@ -1,10 +1,13 @@
 class Solution {
     public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
         Map<Integer,Integer> map=new TreeMap<Integer,Integer>();
+        int c=0;
         for(int i=0;i<capacity.length;i++)
         {
             int diff=(capacity[i]-rocks[i]);
-            if(map.containsKey(diff))
+            if(diff>0)
+            {
+             if(map.containsKey(diff))
             {
                 int count=map.get(diff);
                 map.put(diff,count+1);
@@ -12,21 +15,31 @@ class Solution {
             else
             {
                 map.put(diff,1);
+            }   
+            }
+            else
+            {
+                c++;
             }
         }
-        int c=0;
         for(Map.Entry<Integer,Integer> ele:map.entrySet())
         {
             int i=ele.getValue();
+            if((additionalRocks-(i*ele.getKey()))>=0)
+            {
+                additionalRocks-=(i*ele.getKey());
+                c+=i;
+                continue;
+            }
             while(i-->0)
             {
                 if(additionalRocks>0)
                 {
-                    if(ele.getKey()>=0 && additionalRocks-ele.getKey()>=0)
-                    {
+                    if(ele.getKey()>0 && (additionalRocks-ele.getKey())>=0)
+                    {
                         additionalRocks-=ele.getKey();
                         c++;
-                    }
+                    }
                     else{
                         return c;
                     }
